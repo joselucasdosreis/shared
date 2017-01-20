c43deb12189d95b8338c473fdbd004b290abdd78
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-01-20 18:06:27 -0200

    configuracao (unmodifiable)

---
+++ config/src/main/java/com/github/kyriosdata/healthdb/config/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Implementação de classe que mantém configuração de programa
+ * baseada em conjunto de propriedades (chave/valor) depositadas
+ * em um arquivo texto.
+ */
+package com.github.kyriosdata.healthdb.config;
