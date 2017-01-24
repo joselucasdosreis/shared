02bbeb7c2cd205941d010c86755eb3a79c1a47f5
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-01-24 09:32:29 -0200

    objeto

--- object/src/main/java/com/github/kyriosdata/hdb/object/Campo.java
+++ object/src/main/java/com/github/kyriosdata/hdb/object/Campo.java
@@ -1,12 +1,23 @@
 package com.github.kyriosdata.hdb.object;
 
 /**
- * Informações sobre um campo de uma classe.
+ * Retém informações sobre um campo em um determinado
+ * tipo de registro. Ou seja, não existe a definição
+ * de um campo sem a existência do formato (tipo) do
+ * registro em questão.
  */
 public class Campo {
 
+    /**
+     * Indica valor numérico inteiro de 8 bits.
+     */
     public static int BYTE = 0;
+
+    /**
+     * Indica valor numérico inteiro de 32 bits.
+     */
     public static int INT = 0;
+
     public static int INT64 = 0;
     public static int REAL = 0;
     public static int DOUBLE = 0;
@@ -24,11 +35,26 @@ public class Campo {
     public static int SET = 0;
     public static int HASH = 0;
 
-    public String nome;
-    public int tipo;
-    public int offset;
+    public final String nome;
+    public final int tipo;
+    public final int offset;
 
-    public Campo(String a, int INT, int offset) {
-
+    /**
+     * Cria instância de campo.
+     *
+     * @param identificador O identificador (nome) do campo.
+     *
+     * @param tipo O valor único que identifica o tipo do campo.
+     *
+     * @param offset O deslocamento do campo, em bytes, a partir do
+     *               início do registro no qual está inserido. O valor
+     *               {@link Constantes#INDEFINIDO} indica que o deslocamento
+     *               depende dos valores do registro e, portanto, devem ser
+     *               obtidos do <i>header</i> correspondente.
+     */
+    public Campo(String identificador, int tipo, int offset) {
+        this.nome = identificador;
+        this.tipo = tipo;
+        this.offset = offset;
     }
 }
---
+++ object/src/main/java/com/github/kyriosdata/hdb/object/Constantes.java
@@ -0,0 +1,13 @@
+package com.github.kyriosdata.hdb.object;
+
+/**
+ * Created by kyriosdata on 1/24/17.
+ */
+public class Constantes {
+
+    /**
+     * Valor empregado para indicar que o real valor não
+     * está definido, ou é desconhecido.
+     */
+    public static final int INDEFINIDO = -1;
+}
